0a42ac002d0691d7231c93bbecf7d2876e92dd1b
dbenitez_bcn <dbr.bcn@gmail.com>
2021-04-23 12:06:08 +0200

    creates pokemon prize

---
+++ src/main/java/PokemonPrize.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class PokemonPrize {
+    private final String pokemons[] = new String[] {"Bulbasur", "Charmander", "Squirtle", "Pikachu"};
+
+    @Override
+    public String toString() {
+        final var i = new Random().nextInt(pokemons.length - 1 + 1) + 1;
+        return pokemons[i];
+    }
+}
